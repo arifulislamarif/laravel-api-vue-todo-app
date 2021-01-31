def9f35679e77c24f570a635a590fcff0e2aaad0
Ariful Islam Arif <arif14arif15@gmail.com>
2021-01-31 15:01:44 +0600

    student add done student delete done

--- src/views/Student.vue
+++ src/views/Student.vue
@@ -18,29 +18,24 @@
                   <th width="30%">Action</th>
                 </tr>
               </thead>
-              <tbody>
+              <tbody v-if="students.length != 0">
                 <tr v-for="(student, index) in students" :key="index">
                   <td>{{ index + 1 }}</td>
                   <td>{{ student.name }}</td>
                   <td>{{ student.roll }}</td>
                   <td>{{ student.address }}</td>
                   <td>
-                    <button class="btn btn-primary m-1">Show</button>
+                    <button @click="showStudent(student)" class="btn btn-primary m-1">
+                      Show
+                    </button>
                     <button class="btn btn-info m-1">Edit</button>
-                    <button class="btn btn-danger m-1">Delete</button>
-                  </td>
-                </tr>
-              </tbody>
-              <!-- <tbody v-if="students.length != 0">
-                <tr v-for="(student, index) in students" :key="index">
-                  <td>{{ index + 1 }}</td>
-                  <td>{{ student.name }}</td>
-                  <td>{{ student.roll }}</td>
-                  <td>{{ student.address }}</td>
-                  <td>
-                    <button class="btn btn-primary m-1">Show</button>
-                    <button class="btn btn-info m-1">Edit</button>
-                    <button class="btn btn-danger m-1">Delete</button>
+                    <button
+                      onclick="return confirm('Are you sure you want to delete this item?');"
+                      @click="deleteStudent(student.id)"
+                      class="btn btn-danger m-1"
+                    >
+                      Delete
+                    </button>
                   </td>
                 </tr>
               </tbody>
@@ -48,7 +43,7 @@
                 <tr class="text-center">
                   Nothing Found
                 </tr>
-              </tbody> -->
+              </tbody>
             </table>
           </div>
         </div>
@@ -99,6 +94,66 @@
       </div>
     </div>
   </div>
+
+  <!-- Show Modal -->
+  <div
+    class="modal fade show"
+    style="display: block"
+    v-show="showForm.showModal"
+    data-backdrop="static"
+    data-keyboard="false"
+    tabindex="-1"
+    aria-labelledby="staticBackdropLabel"
+    aria-hidden="true"
+  >
+    <div class="modal-dialog">
+      <div class="modal-content">
+        <div class="modal-header">
+          <h5 class="modal-title" id="staticBackdropLabel">Modal title</h5>
+          <button
+            @click="showForm.showModal = false"
+            type="button"
+            class="close"
+            data-dismiss="modal"
+            aria-label="Close"
+          >
+            <span aria-hidden="true">&times;</span>
+          </button>
+        </div>
+        <div class="modal-body">
+          <form class="text-left">
+            <div class="form-group">
+              <label>Name</label>
+              <input v-model="showForm.name" type="text" class="form-control" disabled />
+            </div>
+            <div class="form-group">
+              <label>Roll</label>
+              <input v-model="showForm.roll" type="text" class="form-control" disabled />
+            </div>
+            <div class="form-group">
+              <label>Address</label>
+              <textarea
+                v-model="showForm.address"
+                class="form-control"
+                rows="5"
+                disabled
+              ></textarea>
+            </div>
+          </form>
+        </div>
+        <div class="modal-footer">
+          <button
+            @click="showForm.showModal = false"
+            type="button"
+            class="btn btn-secondary"
+            data-dismiss="modal"
+          >
+            Close
+          </button>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
@@ -114,53 +169,61 @@ export default {
       address: "",
     });
 
+    const showForm = reactive({
+      name: "",
+      roll: "",
+      address: "",
+      showModal: false,
+    });
+
     const store = useStore();
     const students = computed(() => store.state.students);
     const loadStudents = onMounted(() => store.dispatch("loadStudents"));
 
-    // function addStudentForm() {
-    //   if (form.name && form.roll && form.address) {
-    //     axios
-    //       .post("http://127.0.0.1:8000/api/student", {
-    //         name: form.name,
-    //         roll: form.roll,
-    //         address: form.address,
-    //       })
-    //       .then((response) => {
-    //         console.log(response);
-    //       });
-    //     form.name = "";
-    //     form.roll = "";
-    //     form.address = "";
-
-    //     this.$store.dispatch("loadStudents");
-    //   }
-    // }
-
-    return {
-      form,
-      students,
-      // addStudentForm,
-      loadStudents,
-    };
-  },
-  methods: {
-    addStudentForm() {
+    function addStudentForm() {
       axios
         .post("http://127.0.0.1:8000/api/student", {
-          name: this.form.name,
-          roll: this.form.roll,
-          address: this.form.address,
+          name: form.name,
+          roll: form.roll,
+          address: form.address,
         })
         .then((response) => {
           console.log(response);
+          form.name = "";
+          form.roll = "";
+          form.address = "";
+          store.dispatch("loadStudents");
         });
-      this.form.name = "";
-      this.form.roll = "";
-      this.form.address = "";
+    }
 
-      this.$store.dispatch("loadStudents");
-    },
+    function showStudent(student) {
+      showForm.showModal = true;
+      showForm.name = student.name;
+      showForm.roll = student.roll;
+      showForm.address = student.address;
+
+      // axios.delete(`http://127.0.0.1:8000/api/student/${id}`).then((response) => {
+      //   console.log(response);
+      //   store.dispatch("loadStudents");
+      // });
+    }
+
+    function deleteStudent(id) {
+      axios.delete(`http://127.0.0.1:8000/api/student/${id}`).then((response) => {
+        console.log(response);
+        store.dispatch("loadStudents");
+      });
+    }
+
+    return {
+      form,
+      showForm,
+      students,
+      loadStudents,
+      addStudentForm,
+      showStudent,
+      deleteStudent,
+    };
   },
 };
 </script>
